4f19884e35a15eea685557bfe139ef6d133db4bb
Thabani Takwena <thabanitakwena6@gmail.com>
2022-07-17 20:00:41 +0200

    Game logic

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.addEventListener('load', () => {
     const prev = document.getElementById('prev');
     const next = document.getElementById('next');
     const checkBtn = document.getElementById('check');
+    const riddleNumber = document.getElementById('riddle-number')
     const riddle = document.getElementById('riddle');
 
     //custom variables
@@ -12,6 +13,8 @@ window.addEventListener('load', () => {
     function showRiddle() {
         if (riddleIndex > 20) riddleIndex = 20;
         else if (riddleIndex < 1) riddleIndex = 1;
+
+        riddleNumber.innerHTML = '#' + riddleIndex;
         riddle.innerHTML = riddles[riddleIndex].riddle;
     }
     showRiddle();
@@ -25,12 +28,20 @@ window.addEventListener('load', () => {
         showRiddle();
     }
     checkBtn.onclick = () => {
-        let userAnswer = document.getElementById('input').value()
-        if (userAnswer == riddles[riddleIndex].answer) {
+        let userAnswer = document.getElementById('input').value
+        if (userAnswer == '') {
+            alert('Please fill in text-box!');
+        }else if (userAnswer == riddles[riddleIndex].answer) {
             alert('CORRECT!');
         }else {
             alert('WRONG!');
         }
     }
+
+    window.addEventListener('mousedown', (e) => {
+        if (e.button == 2) {
+            alert("If you are trying to copy and paste the riddle elsewhere to cheat then think again!");
+        }
+    })
    
 });
