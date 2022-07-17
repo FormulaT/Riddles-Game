32e234c3f87109e11dcfa15405b2dd6d547ded1a
Thabani Takwena <thabanitakwena6@gmail.com>
2022-07-17 12:36:12 +0200

    More setting up

--- source files/script.js
+++ source files/script.js
@@ -1,3 +1,14 @@
+const fs = require('fs');
+
+let riddles;
+
+function getJSON() {
+    jsonFile = fs.readFile('riddles.json');
+    console.log(jsonFile)
+    return jsonFile;
+}
+
 window.addEventListener('load', () => {
-    
+    riddles = getJSON();
+    console.log(riddles);    
 });
